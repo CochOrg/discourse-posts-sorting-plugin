333005b9e8ed8d299384185c58a858db616688a5
su1gen <prybylovychav@gmail.com>
2024-03-12 17:57:39 +0200

    Update

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-posts-sorting-plugin
 # about: Updates posts sorting based on created_at field
-# version: 0.0.1
+# version: 0.0.2
 # authors: Awesome Plugin Developer
 # url: https://github.com/CochOrg/discourse-posts-sorting-plugin
 
@@ -15,11 +15,9 @@ after_initialize do
 
   %w[
     lib/posts_sorting/topic_view_extension.rb
-    lib/posts_sorting/reviewable_queued_post_extension.rb
   ].each { |path| require_relative path }
 
   reloadable_patch do
     TopicView.class_eval { prepend DiscoursePostsSorting::TopicViewExtension }
-    ReviewableQueuedPost.class_eval { prepend DiscoursePostsSorting::ReviewableQueuedPostExtension }
   end
 end
